804a5712170f2a5071d9b892c4d1d972b26b8028
unknown <elhadjoumartall10@gmail.com>
2024-07-23 00:04:48 +0200

    Ajout de la methode post

--- mern-server/index.js
+++ mern-server/index.js
@@ -37,6 +37,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // create a collectio of documents
+    const bookCollection = client.db("BookInventory").collection("books");
+
+    // Insert a book to the database : post method
+    app.post("/upload-book", async (req, res) => {
+      const data = req.body;
+      const result = await bookCollection.insertOne(data);
+      res.send(result);
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
